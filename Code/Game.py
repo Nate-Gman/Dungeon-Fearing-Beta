d5eea7468e9cca925b6a9d6e0ee112b7468182df
Mark Andrew Gerads <MarkAndrewGerads.Nazgand@Gmail.Com>
2021-04-24 07:39:10 -0700

    Update realm code

--- Code/Game.py
+++ Code/Game.py
@@ -6,7 +6,7 @@
 
 #Imports
 import pygame, sys, math, random, time
-import Animations, Sprites as sp
+import Animations, Sprites as sp, Realms as r
 
 #Initialzing
 pygame.init()
@@ -50,183 +50,172 @@ selectRect = pygame.Rect(0,0,0,0)
 showSelectRect = False
 
 #Sprites Groups
-Realm = 0
-numberOfRealms = 5
-
 hudSprites = pygame.sprite.Group()
 selectedSprites = pygame.sprite.Group()
 clickable = pygame.sprite.Group()
 
-gameSprites = []
-Walls = []
-Fountains = []
-enemies = []
-allies = []
-
-for k in range(numberOfRealms):
-      gameSprites.append(pygame.sprite.Group())
-      Walls.append(pygame.sprite.Group())
-      Fountains.append(pygame.sprite.Group())
-      enemies.append(pygame.sprite.Group())
-      allies.append(pygame.sprite.Group())
-
 #Sprites
+cr = r.Realm()
+
 E1 = sp.Goblin(160,520)
-gameSprites[0].add(E1)
-enemies[0].add(E1)
+cr.gameSprites.add(E1)
+cr.enemies.add(E1)
 
 E2 = sp.Demon(200,720)
-gameSprites[0].add(E2)
-enemies[0].add(E2)
+cr.gameSprites.add(E2)
+cr.enemies.add(E2)
 
 WF = sp.Fountain(800, 800)
-gameSprites[0].add(WF)
-Fountains[0].add(WF)
+cr.gameSprites.add(WF)
+cr.Fountains.add(WF)
 
 H1 = sp.HPotion(500, 500)
-gameSprites[0].add(H1)
+cr.gameSprites.add(H1)
 
 M1 = sp.MPotion(500, 700)
-gameSprites[0].add(M1)
+cr.gameSprites.add(M1)
 
 C1 = sp.Chest(200, 300)
-gameSprites[0].add(C1)
+cr.gameSprites.add(C1)
 
 FS = sp.FloorSpike(700,600)
-gameSprites[0].add(FS)
+cr.gameSprites.add(FS)
 
 FSS = sp.FloorSpikeSafer(1000,1000)
-gameSprites[0].add(FSS)
+cr.gameSprites.add(FSS)
 
 #Walls
 for k in range(10):
       WL = sp.Wall("Wall",k*80,0)
-      gameSprites[0].add(WL)
-      Walls[0].add(WL)
+      cr.gameSprites.add(WL)
+      cr.Walls.add(WL)
 
 WL = sp.Wall("WallCL",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WallCR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WallCLR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WallLR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallLR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallL",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 for k in range(8):
       WL = sp.Wall("BWallFill",k*80+80,-40)
-      gameSprites[0].add(WL)
-      Walls[0].add(WL)
+      cr.gameSprites.add(WL)
+      cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCLTR",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 for k in range(8):
       WL = sp.Wall("BWallTop",k*80+80,-80)
-      gameSprites[0].add(WL)
-      Walls[0].add(WL)
+      cr.gameSprites.add(WL)
+      cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCRT",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCLT",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCRB",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCLB",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("BWallCLRB",0,0)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 ##Finished_Walls
 WL = sp.Wall("WA1",-1300,400)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA2",-900,-200)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA3",-900,-400)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA4",-500,-1200)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA5",-500,-900)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA6",-500,700)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA7",-200,-200)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA8",-500,-500)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
 
 WL = sp.Wall("WA9",-1000,-1000)
-gameSprites[0].add(WL)
-Walls[0].add(WL)
+cr.gameSprites.add(WL)
+cr.Walls.add(WL)
+
+#
+PI = sp.Inventory(1200, 1200)
+cr.gameSprites.add(PI)
+PS = sp.Skills(-700, -700)
+cr.gameSprites.add(PS)
+##
+XP = sp.Experience(900,760)
+cr.gameSprites.add(XP)
+
+HP = sp.HitPoints(900,800)
+cr.gameSprites.add(HP)
+
+MP = sp.ManaPoints(920,820)
+cr.gameSprites.add(MP)
+#Player
+MainCharacter = sp.Player(99,99)
+cr.gameSprites.add(MainCharacter)
+cr.allies.add(MainCharacter)
+selectedSprites.add(MainCharacter)
 
 ##hudSprites
 PD = sp.Displayer(0, 100)
 hudSprites.add(PD)
-#
-PI = sp.Inventory(1200, 1200)
-gameSprites[0].add(PI)
-PS = sp.Skills(-700, -700)
-gameSprites[0].add(PS)
-##
-XP = sp.Experience(900,760)
-gameSprites[0].add(XP)
-
-HP = sp.HitPoints(900,800)
-gameSprites[0].add(HP)
-
-MP = sp.ManaPoints(920,820)
-gameSprites[0].add(MP)
-#Player
-MainCharacter = sp.Player(99,99)
-gameSprites[0].add(MainCharacter)
-allies[0].add(MainCharacter)
-selectedSprites.add(MainCharacter)
 
 def teleportAllSprites(east, south):
-      for sprite in gameSprites[Realm]:
+      if east == 0 and south == 0: #Sometimes save computation
+            return
+      for sprite in cr.gameSprites:
             sprite.rect.left += east
             sprite.rect.top += south
 
@@ -247,11 +236,11 @@ def centerOfSelectedSprites():
 while True:
       DISPLAYSURF.fill(FloorColor)
       #Moves and Re-draws all Sprites
-      for sprite in gameSprites[Realm]:
+      for sprite in cr.gameSprites:
             sprite.draw(DISPLAYSURF)
             sprite.move()
             #Walls push sprites around, thus walls will push walls around
-            for wall in Walls[Realm]:
+            for wall in cr.Walls:
                   if pygame.sprite.collide_rect(wall, sprite):
                         WX,WY = wall.rect.center
                         sX,sY = sprite.rect.center
@@ -260,20 +249,20 @@ while True:
                         sprite.rect.move_ip(MoveX, MoveY)
 
       #Fountain
-      for ally in allies[Realm]:
-            for Fountain in Fountains[Realm]:
+      for ally in cr.allies:
+            for Fountain in cr.Fountains:
                   if pygame.sprite.collide_rect(ally, Fountain):
                         ally.heal(Fountain.healBuff)
       #Fighting
-      for ally in allies[Realm]:
-            for enemy in enemies[Realm]:
+      for ally in cr.allies:
+            for enemy in cr.enemies:
                   if pygame.sprite.collide_rect(ally, enemy):
                         ally.meleeFight(enemy)
       #Check deaths
-      for ally in allies[Realm].copy():
+      for ally in cr.allies.copy():
             if ally.health <= 0:
                   ally.kill()
-      for enemy in enemies[Realm].copy():
+      for enemy in cr.enemies.copy():
             if enemy.health <= 0:
                   enemy.kill()
  
@@ -332,7 +321,7 @@ while True:
                         if rightClickMode == "SelectSprites":
                               showSelectRect = False
                               newSelectedSprites = pygame.sprite.Group()
-                              for sprite in gameSprites[Realm]:
+                              for sprite in cr.gameSprites:
                                     if selectRect.colliderect(sprite.rect):
                                           newSelectedSprites.add(sprite)
                               if not pressed_keys[pygame.K_LCTRL]:
--- Code/Realms.py
+++ Code/Realms.py
@@ -1,2 +1,9 @@
 import pygame
 
+class Realm():
+    def __init__(self):
+        self.gameSprites = pygame.sprite.Group()
+        self.Walls = pygame.sprite.Group()
+        self.Fountains = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.allies = pygame.sprite.Group()
